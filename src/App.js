22445319a3503b1a01f87960c13c9b0eb20dfb55
YakasiriRavi <raviyakasiri77@gmail.com>
2023-09-16 00:20:27 +0530

    files to repo.
    set up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {useState} from "react";
+import './App.css';
+import Parent from "./parentComp/Parent";
+function App() {
+  // BABEL will convert JSX TO VANILLA JS OR PLAIN JS---> THIS WILL UNDERSTOOD BY THE BROWSER.
+const [initialState, setInitialState]=useState("Seshu")
+const [value, setValue]=useState("")
+  return (
+    <div className="App">
+      <h1>Parent Section</h1>
+      <Parent/>
+    </div>
+  );
+}
+
+export default App;
